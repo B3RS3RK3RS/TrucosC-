920e634f311a9963ccd966b6e0b053af68c17252
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-09-28 23:00:04 -0500

    BASICO: Operador Ternario 2 - Concatenación y Operaciones

--- Trucos CSharp/Operador Ternario/Program.cs
+++ Trucos CSharp/Operador Ternario/Program.cs
@@ -17,10 +17,23 @@ namespace Operador_Ternario
                  autorizado = false;*/
 
             //autorizado = edad >= 18 ? true : false;
-            autorizado = edad >= 18;
+            //autorizado = edad >= 18;
 
-            string mensaje = autorizado ? "Usuario autorizado" : "Usuario no autorizado";
-            Console.WriteLine(mensaje);
+            //string mensaje = autorizado ? "Usuario autorizado" : "Usuario no autorizado";
+            //Console.WriteLine(mensaje);
+
+            //CONCATENAMOS LOS OPERADORES TERNARIOS
+            string mensaje2 = edad >= 18 ? "Usuario autorizado" :
+                edad >= 12 ? "Usuario con restricciones" : "Usuario no autorizado";
+            Console.WriteLine(mensaje2);
+
+
+            //DENTRO DEL OPERADOR SE PUEDEN REALIZAR OPERACIONES
+            Console.WriteLine("Ingrese un entero");
+            var num = Convert.ToInt32(Console.ReadLine());
+            int op = 2;
+            double resp = op == 1 ? Math.Sin(num) : Math.Cos(num);
+            Console.WriteLine($"Operacion {op} de {num} = {resp}");
         }
     }
 }
