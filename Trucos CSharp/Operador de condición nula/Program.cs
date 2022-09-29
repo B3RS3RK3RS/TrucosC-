5bd238e5de66f7c8a619e41002483dc6e599ab2b
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-09-28 23:30:25 -0500

    BASICO: Operador Condicional Nulo

---
+++ Trucos CSharp/Operador de condición nula/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operador_de_condición_nula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int?[] nums = new int?[] { null, null, 10, null, 100 };
+            string mensaje;
+            int? item = nums[2];
+            mensaje = item == null ? "No se encuentra entero" : item.ToString();
+            Console.WriteLine(mensaje);
+
+            var mensaje2 = item?.ToString() ?? "item nulo";
+            Console.WriteLine(mensaje2);
+        }
+    }
+}
