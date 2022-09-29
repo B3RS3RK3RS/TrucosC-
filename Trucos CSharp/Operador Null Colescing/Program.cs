feb2837e2415fe147c2f2bdadf255f91da793017
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-09-28 23:23:10 -0500

    BASICO: Operador Null Colescring 2 - Manejando excepciones

--- Trucos CSharp/Operador Null Colescing/Program.cs
+++ Trucos CSharp/Operador Null Colescing/Program.cs
@@ -7,13 +7,29 @@ namespace Operador_Null_Colescing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
             int? x = null;  //DECLARAMOS X COMO NULO (los int no pueden ser nulo, pero cambiamos la condicion al colocar ?)
             int nuevo = x ?? 0; //Asignamos un valor de 0 a nuevo si "x" es nulo
 
-            int?[] nums = new int?[] { 1, null, null };
-            int suma = nums.Sum() ?? 0;
-            Console.WriteLine($"La suma es: {suma}");
+            //int?[] nums = new int?[] { 1, null, null };
+            //int suma = nums.Sum() ?? 0;
+            //Console.WriteLine($"La suma es: {suma}");
+
+            Console.WriteLine("Escribe tu nombre");
+            string nombre = Console.ReadLine();
+            var usuario = new Usuario { Nombre = nombre, Apellido = null};
+        }
+    }
+
+    public class Usuario
+    {
+        private string apellido;
+        public string Nombre { get; set; }
+
+        public string Apellido
+        {
+            get => apellido;
+            set => apellido = value ?? throw new ArgumentNullException(nameof(value), "El apellido no puede ser nulo");
         }
     }
 }
