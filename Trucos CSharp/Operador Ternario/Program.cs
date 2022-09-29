d03b1b2119e7c018d4e415b8623f33bca8a8146f
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-09-28 22:50:23 -0500

    BASICO: Operador Ternario

---
+++ Trucos CSharp/Operador Ternario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+//SINTAXIS:
+// condicion ? expresion A : expresion B
+namespace Operador_Ternario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            bool autorizado = false;
+            /* if (edad >= 18)
+                 autorizado = true;
+             else
+                 autorizado = false;*/
+
+            //autorizado = edad >= 18 ? true : false;
+            autorizado = edad >= 18;
+
+            string mensaje = autorizado ? "Usuario autorizado" : "Usuario no autorizado";
+            Console.WriteLine(mensaje);
+        }
+    }
+}
