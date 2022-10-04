8ba6fb3c4892b5a1e3b3c9eccfaa22a28f979b82
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-10-04 13:21:18 -0500

    INTERMEDIO: Validación de Cadenas

---
+++ Trucos CSharp/Validacion de Cadenas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Validacion_de_Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Escriba su nombre");
+                nombre = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(nombre));
+            //} while (string.IsNullOrEmpty(nombre));
+        }
+    }
+}
