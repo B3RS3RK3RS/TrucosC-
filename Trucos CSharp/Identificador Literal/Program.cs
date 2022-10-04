6c27d8c2514ded5df820e77cfbd28042eabf8ac1
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-10-04 16:04:03 -0500

    AVANZADO: Identificador Literal @

---
+++ Trucos CSharp/Identificador Literal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Identificador_Literal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"D:\Archivos\UTP\VIII CICLO\INTEGRADOR I\GIT\C# Trucos\TrucosC-\Trucos CSharp\Identificador Literal\Program.cs";
+            Console.WriteLine(ruta);
+        }
+    }
+}
---
+++ Trucos CSharp/Identificadores Literales/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Identificadores_Literales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"D:\Archivos\UTP\VIII CICLO\INTEGRADOR I\GIT\C# Trucos\TrucosC-\Trucos CSharp\Identificador Literal\Program.cs";
+            Console.WriteLine(ruta);
+
+            string @if = "if(a>b){\n .. \n}";
+            Console.WriteLine(@if);
+        }
+    }
+}
