962142ee24482bac2f8aafb37dc932c3cc5ebb3d
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-10-04 13:17:40 -0500

    INTERMEDIO: Interpolación de Cadenas

---
+++ Trucos CSharp/Interpolacion de Cadenas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interpolacion_de_Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Gustavo";
+            DateTime dt = DateTime.Now;
+
+            //FORMATO DE COMPOSICION
+            Console.WriteLine("Nombre: {0}, Fecha: {1}", nombre, dt);
+            //FORMATO DE INTERPOLACION
+            Console.WriteLine($"Nombre: {nombre}, Fecha: {dt}");
+
+            //TAMBIEN SE PUEDEN HACER CALCULOS DIRECTAMENTE
+            Console.WriteLine($"Redondeo: {Math.Round(5.6)}");
+        }
+    }
+}
