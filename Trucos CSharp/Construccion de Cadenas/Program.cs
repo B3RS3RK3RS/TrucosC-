82794b1c2bab9bef7aa7e7d06bb23c30e973b137
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-10-04 13:06:27 -0500

    INTERMEDIO: Cadenas - Comparacion entre string y StringBuilder

---
+++ Trucos CSharp/Construccion de Cadenas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Construccion_de_Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensaje = "Hola mundo";
+            mensaje = "hola nuevamente mundo";
+
+
+            StringBuilder sb = new StringBuilder("Hola Mundo");
+            StringBuilder sb2 = new StringBuilder(5, 5); //CAPACIDAD, CAPACIDAD MAXIMA
+            //sb2.Append(sb);
+
+
+            //Comparacion entre string y StringBuilder
+            Stopwatch sw = new Stopwatch();
+            int ciclos = 50000;
+
+            string str = "";
+            sw.Start();
+            for (int i = 0; i < ciclos; i++)
+            {
+                str +=i.ToString();
+            }
+            sw.Stop();
+            Console.WriteLine($"TIEMPO DE STRING EN MILISEGUNDOS {sw.ElapsedMilliseconds}");
+
+
+            StringBuilder sw3 = new StringBuilder();
+            sw.Restart();
+            for (int i = 0; i < ciclos; i++)
+            {
+                sw3.Append(i.ToString());
+            }
+            sw.Stop();
+            Console.WriteLine($"TIEMPO DE STRINGBUILDER EN MILISEGUNDOS {sw.ElapsedMilliseconds}");
+        }
+    }
+}
