643a77498e685f1a3ff4d9faab4c2fcb7b457e30
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-10-04 14:03:14 -0500

    INTERMEDIO: Validacion de cadenas 3 - StartsWith y EndsWith

--- Trucos CSharp/Validacion de Cadenas/Program.cs
+++ Trucos CSharp/Validacion de Cadenas/Program.cs
@@ -6,24 +6,40 @@ namespace Validacion_de_Cadenas
     {
         static void Main(string[] args)
         {
-            string nombre = string.Empty;
+            //string nombre = string.Empty;
 
-            do
+            //do
+            //{
+            //    Console.WriteLine("Escriba su nombre");
+            //    nombre = Console.ReadLine();
+            //} while (string.IsNullOrWhiteSpace(nombre));
+            ////} while (string.IsNullOrEmpty(nombre));
+
+            //string subc = "tavo";
+            //if (nombre.Contains(subc))
+            //{
+            //    var i = nombre.IndexOf(subc);   //PARA SABER LA POSICION DE LA SUBCADENA
+            //    Console.WriteLine($"El nombre contiene la subcadena en la posición: {i}");
+            //}
+
+
+            //COMPARAR UNA ENTRADA CON UNA CADENA Y VER SI COINCIDE EN EL INICIO O FIN
+            string entrada = "GGCG";
+            string cadena = "GGcgCGUGGGCUAGCGCCACUCAAAAGGCCCAU";
+
+            int coincidenciaInicio = 0;
+            int coincidenciaFin = 0;
+
+            for (int i = 0; i < cadena.Length - 1; i += 4)
             {
-                Console.WriteLine("Escriba su nombre");
-                nombre = Console.ReadLine();
-            } while (string.IsNullOrWhiteSpace(nombre));
-            //} while (string.IsNullOrEmpty(nombre));
-
-
-
-
-            string subc = "tavo";
-            if (nombre.Contains(subc))
-            {
-                var i = nombre.IndexOf(subc);   //PARA SABER LA POSICION DE LA SUBCADENA
-                Console.WriteLine($"El nombre contiene la subcadena en la posición: {i}");
+                string c = cadena.Substring(i, 4);
+                if (c.StartsWith(entrada[0]))
+                    coincidenciaInicio += 1;
+                if (c.EndsWith(entrada[3]))
+                    coincidenciaFin += 1;
             }
+            Console.WriteLine($"Inicio coincide en: {coincidenciaInicio} veces");
+            Console.WriteLine($"Inicio coincide en: {coincidenciaFin} veces");
         }
     }
 }
