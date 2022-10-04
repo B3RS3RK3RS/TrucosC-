c136d5c157c13769bcc9ab144f8f2fa27e989715
Gustavo Fernández Saavedra <gustavofernandezsaavedra@gmail.com>
2022-10-04 18:09:58 -0500

    AVANZADO: Directivas - Region - warning, pragma, if

---
+++ Trucos CSharp/Directivas del preProcesador/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Directivas_del_preProcesador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DIVIDIR CODIGO EN REGIONES
+            #region Directivas
+
+//COMO IF
+#if DEBUG
+            Console.WriteLine("Hello World!");
+//COMO ELSE IF
+#elif RELEASE
+            GuardarEnBD();
+            //COMO END IF
+#endif
+
+//PARA DESHABILITAR ALERTAS DE VARIABLES NO USADAS
+#pragma warning disable
+            var x = "Hola";
+
+//PARA NUEVAMENTA HABILITAR ALERTAS DE VARIABLES NO USADAS
+#pragma warning restore
+            var x2 = "Hola";
+
+
+#warning EN DESUSO
+            var prueba = "dsd";
+
+
+            //FINALIZAR REGION
+            #endregion
+        }
+
+        public static void GuardarEnBD()
+        {
+            Console.WriteLine("estoy en bd");
+        }
+    }
+}
